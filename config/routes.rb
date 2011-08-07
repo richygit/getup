7df4438310c683e72a27d663ddb8112acb4548e5
richy y <richard.yuwono@gmail.com>
2011-08-07 16:18:04 +1000

    fixed root route.

--- config/routes.rb
+++ config/routes.rb
@@ -52,7 +52,7 @@ Getup::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "search#index"
+  root :to => "people#index"
 
   # See how all your routes lay out with "rake routes"
 
