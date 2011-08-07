f5a3675aef2f80341aa06610df037963002db089
richy y <richard.yuwono@gmail.com>
2011-08-08 01:19:26 +1000

    sanitise params for db queries.

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,7 +5,6 @@ class PeopleController < ApplicationController
     @email_domains = People.email_domains
 
     if(params[:email_option] || params[:postcode_option])
-      #TODO sanitise params
       @people = People.do_search(params[:email_option], params[:email], params[:postcode_option], params[:postcode])
     end
   end
--- app/models/people.rb
+++ app/models/people.rb
@@ -6,11 +6,37 @@ class People < ActiveRecord::Base
     def email_domains
       #TODO memcache?
       People.select(:email_domain).group(:email_domain).order(:email_domain)
-      #People.find_by_sql "SELECT email_domain FROM people GROUP by email_domain"
     end
 
-    #perform search for people based on email or postcode
+    # sanitise the email array for use in db query
+    def sanitise_email(email)
+      if email.nil?
+        return nil
+      end
+
+      email.length.times do |i|
+        email[i] = email[i].gsub(/[^\w]/,'') #remove all non-word chars
+      end
+      return email
+    end
+
+    #sanitise the postcode array for use in db query
+    def sanitise_postcode(postcode)
+      if postcode.nil?
+        return nil
+      end
+
+      postcode.length.times do |i|
+        postcode[i] = postcode[i].gsub(/[^\d]/, '') #remove all non-numeric chars
+      end
+      return postcode
+    end
+
+    # perform search for people based on email or postcode
     def do_search(email_option, email, postcode_option, postcode)
+      email = sanitise_email(email)
+      postcode = sanitise_postcode(postcode)
+
       query = People.joins(:postcode)
       if(email_option == 'include' && email )
         query = query.where(:email_domain => email)
